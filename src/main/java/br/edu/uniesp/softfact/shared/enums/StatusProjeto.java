a1e5fcc463c2f1db7edcb1386e3a3c06a1ff2105
jotavtech <jotavtech@gmail.com>
2025-12-05 10:52:41 -0300

    feat: adiciona enum StatusProjeto para controle de status dos projetos

---
+++ src/main/java/br/edu/uniesp/softfact/shared/enums/StatusProjeto.java
@@ -0,0 +1,11 @@
+package br.edu.uniesp.softfact.shared.enums;
+
+/**
+ * Representa o status atual de um projeto no sistema.
+ */
+public enum StatusProjeto {
+    EM_ANDAMENTO,
+    CONCLUIDO,
+    PAUSADO,
+    CANCELADO
+}
